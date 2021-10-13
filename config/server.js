486e828c02a66be1f36b6aba31b6934ece646020
julio <juli0o79@hotmail.com>
2021-10-13 17:09:04 -0300

    strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7db82d3be593760a8e2795e677950a84'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b99d3539-0ec8-40db-87d8-8be284e12167'
+};
